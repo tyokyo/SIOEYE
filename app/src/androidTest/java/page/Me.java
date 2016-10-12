c95226a4f78c3e847a6549d6606ef7359616e023
tyokyo <qiang.zhang@ck-telecom.com>
2016-10-12 11:23:23 +0800

    添加Me相关的ID

--- app/src/androidTest/java/page/Me.java
+++ app/src/androidTest/java/page/Me.java
@@ -7,6 +7,22 @@ public class Me {
     public static final String ID_HEAD_IMAGE="com.sioeye.sioeyeapp:id/load_headimg";
     public static final String ID_USER_EDIT="com.sioeye.sioeyeapp:id/user_edit";
     public static final String ID_CAMERA_SELECT="org.codeaurora.snapcam:id/btn_done";
-    public static final String ID_IMAGE_CHANGE=" com.sioeye.sioeyeapp:id/llt_layout";
+    public static final String ID_IMAGE_CHANGE="com.sioeye.sioeyeapp:id/llt_layout";
+    public static final String ID_IMAGE_CANCEL="org.codeaurora.snapcam:id/btn_cancel";
+    public static final String CHANGECOVERPHOTO="com.sioeye.sioeyeapp:id/bg_text_hint";
+    public static final String SETNICKNAMECONTENT="com.sioeye.sioeyeapp:id/sample_content";
+    public static final String GETNICKNAMECONTENT="com.sioeye.sioeyeapp:id/content_text";
+
+    //search locations
+    public static final String IS_LOCATING="com.sioeye.sioeyeapp:id/llt_layout";
+    public static final String SEARCH_LOCATE="com.sioeye.sioeyeapp:id/search_filter_input";
+    public static final String LOCATION_NAME="com.sioeye.sioeyeapp:id/location_name";
+    public static final String LOCATION_FAIL="The current location is fail";
+    public static final String LOCATION_LIST="com.sioeye.sioeyeapp:id/location_list";
+
+    //activity ids
+    public static final String ACTIVITY_INTERESTS="com.sioeye.sioeyeapp:id/display_text";
+    public static final String ACTIVITY_IN_INTERESTED="com.sioeye.sioeyeapp:id/tag_text";
+
 
 }
