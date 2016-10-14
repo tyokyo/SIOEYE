69469119c1418e860dcc3800e29530612a875625
tyokyo <qiang.zhang@ck-telecom.com>
2016-10-14 13:43:43 +0800

    update

--- app/src/androidTest/java/testcase/me/NickNameCase.java
+++ app/src/androidTest/java/testcase/me/NickNameCase.java
@@ -22,6 +22,7 @@ import page.Me;
 @RunWith(AndroidJUnit4.class)
 @SdkSuppress(minSdkVersion = 18)
 public class NickNameCase  extends VP2 {
+
     @Before
     public  void setup(){
         openAppByPackageName(App.SIOEYE_PACKAGE_NAME);
