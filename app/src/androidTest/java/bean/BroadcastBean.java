eff2717043ded7c85da73369cfb11363b7fc27ff
tyokyo <qiang.zhang@ck-telecom.com>
2016-10-17 16:56:14 +0800

    Bean file

---
+++ app/src/androidTest/java/bean/BroadcastBean.java
@@ -0,0 +1,50 @@
+package bean;
+
+/**
+ * Created by elon on 2016/10/17.
+ */
+public class BroadcastBean {
+    //com.sioeye.sioeyeapp:id/broadcast_title
+    //com.sioeye.sioeyeapp:id/broadcast_desc
+    //com.sioeye.sioeyeapp:id/broadcast_time
+    //com.sioeye.sioeyeapp:id/broadcast_like
+
+    public String getBroadcast_title() {
+        return broadcast_title;
+    }
+
+    public void setBroadcast_title(String broadcast_title) {
+        this.broadcast_title = broadcast_title;
+    }
+
+    public String getBroadcast_desc() {
+        return broadcast_desc;
+    }
+
+    public void setBroadcast_desc(String broadcast_desc) {
+        this.broadcast_desc = broadcast_desc;
+    }
+
+    public String getBroadcast_like() {
+        return broadcast_like;
+    }
+
+    public void setBroadcast_like(String broadcast_like) {
+        this.broadcast_like = broadcast_like;
+    }
+
+    private String broadcast_title;
+    private String broadcast_desc;
+
+    public String getBroadcast_time() {
+        return broadcast_time;
+    }
+
+    public void setBroadcast_time(String broadcast_time) {
+        this.broadcast_time = broadcast_time;
+    }
+
+    private String broadcast_time;
+    private String broadcast_like;
+
+}
---
+++ app/src/androidTest/java/bean/WatcherBean.java
@@ -0,0 +1,37 @@
+package bean;
+
+/**
+ * Created by elon on 2016/10/17.
+ */
+public class WatcherBean {
+    private String watch;
+
+    public String getWatch() {
+        return watch;
+    }
+
+    public void setWatch(String watch) {
+        this.watch = watch;
+    }
+
+    public String getComments() {
+        return comments;
+    }
+
+    public void setComments(String comments) {
+        this.comments = comments;
+    }
+
+    public String getZan() {
+        return zan;
+    }
+
+    public void setZan(String zan) {
+        this.zan = zan;
+    }
+
+    private String comments;
+    private String zan;
+
+
+}
