8b7f736a695a688f00d76e122cfb9c552e95af0f
tyokyo <qiang.zhang@ck-telecom.com>
2016-10-21 09:56:52 +0800

    spoon执行

---
+++ app/spoon/runDebug.bat
@@ -0,0 +1,2 @@
+java -jar spoon-runner-1.6.2-jar-with-dependencies.jar  --apk ../../app/build/outputs/apk/app-debug.apk --test-apk ../../app/build/outputs/apk/app-debug-androidTest-unaligned.apk --class-name testcase.me.TestDemo  --method-name testA
+
