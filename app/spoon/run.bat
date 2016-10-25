ba149a9acd54564b7431c2cfdfcf993a0171da51
tyokyo <qiang.zhang@ck-telecom.com>
2016-10-25 13:30:15 +0800

    add --grant-all

--- app/spoon/run.bat
+++ app/spoon/run.bat
@@ -1,2 +1,2 @@
-java -jar spoon-runner-1.6.2-jar-with-dependencies.jar  --apk ../../app/build/outputs/apk/app-debug.apk --test-apk ../../app/build/outputs/apk/app-debug-androidTest-unaligned.apk
+java -jar spoon-runner-1.6.2-jar-with-dependencies.jar  --apk ../../app/build/outputs/apk/app-debug.apk --test-apk ../../app/build/outputs/apk/app-debug-androidTest-unaligned.apk --grant-all
 
