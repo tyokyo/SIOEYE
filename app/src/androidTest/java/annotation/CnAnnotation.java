1869b340374c95646c678df602b291750b01d099
tyokyo <qiang.zhang@ck-telecom.com>
2016-11-02 10:49:57 +0800

    qiang.zhang  add SmokeAnnotation

---
+++ app/src/androidTest/java/annotation/CnAnnotation.java
@@ -0,0 +1,14 @@
+package annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Created by elon on 2016/11/2.
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD, ElementType.TYPE})
+public @interface CnAnnotation {
+}
