f23c33150c8a73542713b1d99ae4a08000acd57e
tyokyo <qiang.zhang@ck-telecom.com>
2016-11-02 16:25:59 +0800

    tyokyo add navToActivities

--- app/src/androidTest/java/action/MeAction.java
+++ app/src/androidTest/java/action/MeAction.java
@@ -1,5 +1,10 @@
 package action;
 
+import android.support.test.uiautomator.By;
+import android.support.test.uiautomator.Until;
+
+import com.squareup.spoon.Spoon;
+
 import ckt.base.VP2;
 import page.Me;
 
@@ -10,5 +15,18 @@ public class MeAction extends VP2{
     public static void navToLiveConfiguration(){
         clickByText("Me");
         clickById(Me.LIVE_CONFIGURATION);
+        waitTime(2);
+    }
+    public static void navToNotifications(){
+        clickByText("Me");
+        clickById(Me.NOTIFICATIONS);
+        gDevice.wait(Until.gone(By.res(Me.LOADING_FOLLOWERS)),40000);
+    }
+    public static void navToActivities(){
+        clickByText("Me");
+        Spoon.screenshot(gDevice,"Me");
+        clickById(Me.ID_USER_EDIT);
+        clickByText("Activity");
+        gDevice.wait(Until.gone(By.res(Me.IS_LOCATING)),20000);
     }
 }
