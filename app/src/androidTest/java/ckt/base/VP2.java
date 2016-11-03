2f8ef60133005faa64d00462392c0b59afed26b9
tyokyo <qiang.zhang@ck-telecom.com>
2016-11-03 17:01:53 +0800

    add wait method

--- app/src/androidTest/java/ckt/base/VP2.java
+++ app/src/androidTest/java/ckt/base/VP2.java
@@ -813,4 +813,53 @@ public class VP2 extends  VP{
             e.printStackTrace();
         }
     }
+    public static String base = "abcdefghijklmnopqrstuvwxyz0123456789";
+    private static final String[] email_suffix="@gmail.com,@yahoo.com,@msn.com,@hotmail.com,@aol.com,@ask.com,@live.com,@qq.com,@0355.net,@163.com,@163.net,@263.net,@3721.net,@yeah.net,@googlemail.com,@126.com,@sina.com,@sohu.com,@yahoo.com.cn".split(",");
+
+    public static int getNum(int start,int end) {
+        return (int)(Math.random()*(end-start+1)+start);
+    }
+    /**
+     * 返回Email
+     * @param lMin 最小长度
+     * @param lMax 最大长度
+     * @return
+     */
+    public static String getRandomEmail(int lMin,int lMax) {
+        int length=getNum(lMin,lMax);
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < length; i++) {
+            int number = (int)(Math.random()*base.length());
+            sb.append(base.charAt(number));
+        }
+        sb.append(email_suffix[(int)(Math.random()*email_suffix.length)]);
+        return sb.toString();
+    }
+    /**
+     * 返回手机号码
+     */
+    private static String[] telFirst="134,135,136,137,138,139,150,151,152,157,158,159,130,131,132,155,156,133,153".split(",");
+    public static String getRandomTel() {
+        int index=getNum(0,telFirst.length-1);
+        String first=telFirst[index];
+        String second=String.valueOf(getNum(1,888)+10000).substring(1);
+        String thrid=String.valueOf(getNum(1,9100)+10000).substring(1);
+        return first+second+thrid;
+    }
+    /**
+     * Get the Launcher Package Name.
+     * @param resourceID id
+     * @param timeout timeout for wait
+     */
+    public static void waitUntilFind(String resourceID,int timeout){
+        gDevice.wait(Until.findObject(By.res(resourceID)),timeout);
+    }
+    /**
+     * Get the Launcher Package Name.
+     * @param resourceID id
+     * @param timeout timeout for wait
+     */
+    public static void waitUntilGone(String resourceID,int timeout){
+        gDevice.wait(Until.findObject(By.res(resourceID)),timeout);
+    }
 }
