8d198625f0b5520809a1e8797a8d4a3a68e647e2
tyokyo <qiang.zhang@ck-telecom.com>
2016-11-04 14:25:54 +0800

    constant

--- app/src/androidTest/java/ckt/tools/Constant.java
+++ app/src/androidTest/java/ckt/tools/Constant.java
@@ -7,4 +7,7 @@ public class Constant {
     //sio-eye account
     public static  final  String userName ="tyokyo@126.com";
     public static  final  String passwd   = "123456789";
+
+    //permission allow dialog
+    public static String PERMISSION_ALLOW="com.android.packageinstaller:id/permission_allow_button";
 }
