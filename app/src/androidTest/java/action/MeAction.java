7e77665b2e70d99d4dea5e06cc0a972298b6de33
tyokyo <qiang.zhang@ck-telecom.com>
2016-11-04 09:35:18 +0800

    navToBroadcasts

--- app/src/androidTest/java/action/MeAction.java
+++ app/src/androidTest/java/action/MeAction.java
@@ -13,25 +13,37 @@ import page.Me;
  */
 public class MeAction extends VP2{
     public static void navToLiveConfiguration(){
-        clickByText("Me");
+        clickById(Me.ID_MAIN_TAB_ME);
         clickById(Me.LIVE_CONFIGURATION);
         waitTime(2);
     }
     public static void navToNotifications(){
-        clickByText("Me");
+        clickById(Me.ID_MAIN_TAB_ME);
         clickById(Me.NOTIFICATIONS);
         gDevice.wait(Until.gone(By.res(Me.LOADING_FOLLOWERS)),40000);
     }
     public static void navToActivities(){
-        clickByText("Me");
+        clickById(Me.ID_MAIN_TAB_ME);
         Spoon.screenshot(gDevice,"Me");
         clickById(Me.ID_USER_EDIT);
-        clickByText("Activity");
+        clickById(Me.NAV_EDIT_ACTIVITY);
         gDevice.wait(Until.gone(By.res(Me.IS_LOCATING)),20000);
     }
     public static void navToUserEdit(){
         clickById(Me.ID_MAIN_TAB_ME);
         Spoon.screenshot(gDevice,"Me");
         clickById(Me.ID_USER_EDIT);
+    }
+    public static void navToNickName(){
+        clickById(Me.ID_MAIN_TAB_ME);
+        Spoon.screenshot(gDevice,"Me");
+        clickById(Me.ID_USER_EDIT);
+        clickById(Me.NAV_EDIT_NICKNAME);
+    }
+    public static void navToBroadcasts(){
+        clickById(Me.ID_MAIN_TAB_ME);
+        Spoon.screenshot(gDevice,"Me");
+        clickById(Me.ID_ME_BROADCAST);
+        gDevice.wait(Until.gone(By.res(Me.BROADCAST_VIEW)),20000);
     }
 }
