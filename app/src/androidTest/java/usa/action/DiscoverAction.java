c2251c1c7d3d25421ddedfcd5d82e8d2b6a25e39
caibing.yin <caibing.yin@ckt-telecom>
2016-11-05 18:21:42 +0800

    DiscoverAction

--- app/src/androidTest/java/usa/action/DiscoverAction.java
+++ app/src/androidTest/java/usa/action/DiscoverAction.java
@@ -2,8 +2,21 @@ package usa.action;
 
 import android.support.test.filters.SdkSuppress;
 import android.support.test.runner.AndroidJUnit4;
+import android.support.test.uiautomator.By;
+import android.support.test.uiautomator.UiObject;
+import android.support.test.uiautomator.UiObject2;
+import android.support.test.uiautomator.UiObjectNotFoundException;
+import android.support.test.uiautomator.UiSelector;
+import android.util.Log;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+
+import com.squareup.spoon.Spoon;
+
 import org.junit.Before;
 import org.junit.runner.RunWith;
+
+import java.util.List;
 import java.util.logging.Logger;
 import ckt.base.VP2;
 import usa.page.App;
@@ -14,35 +27,55 @@ import usa.testcase.me.ActivityCase;
  */
 
 public class DiscoverAction extends VP2 {
-    /**
-     * Created by caibing,yin on 2016/11/5.
-     */
-    @RunWith(AndroidJUnit4.class)
-    @SdkSuppress(minSdkVersion = 18)
-    public static class ToDiscoverCase extends VP2 {
-        Logger logger = Logger.getLogger(ActivityCase.class.getName());
-        @Before
-        public  void setup(){
-            openAppByPackageName(App.SIOEYE_PACKAGE_NAME_USA);
+    Logger logger = Logger.getLogger(ActivityCase.class.getName());
+    public void navToDiscover(){
+        clickById(Discover.ID_MAIN_TAB_DISCOVER);
+        waitTime(1);
+        logger.info("navToDiscover");
+    }
+    //
+    public static void navToSearch(){
+        clickById(Discover.ID_MAIN_TAB_DISCOVER);
+        click_search_btn();
+    }
+    //
+    private static void click_search_btn(){
+        List<UiObject2> frameLayout_lists =gDevice.findObjects(By.clazz(FrameLayout.class));
+        for (UiObject2 frameLayout:frameLayout_lists) {
+            boolean sio_eye = frameLayout.hasObject(By.res("com.sioeye.sioeyeapp:id/title"));
+            if (sio_eye){
+                UiObject2 search_btn=frameLayout.findObject(By.clazz(ImageView.class));
+                search_btn.click();
+                break;
+            }
         }
-        public void navToDiscover(){
-            clickById(Discover.ID_MAIN_TAB_DISCOVER);
-            waitTime(1);
-        }
-        public void navToSearch(){
-            clickById(Discover.ID_MAIN_TAB_DISCOVER);
-        }
-        public void navToAd(){
-            clickById(Discover.ID_MAIN_TAB_DISCOVER);
-        }
-        public void navToRecommendList(){
-            clickById(Discover.ID_MAIN_TAB_DISCOVER);
-        }
-        public void scrollAdSplash(){
-            clickById(Discover.ID_MAIN_TAB_DISCOVER);
-        }
-        public void scrollRecommendList(){
-            clickById(Discover.ID_MAIN_TAB_DISCOVER);
+    }
+    public void navToAd() throws UiObjectNotFoundException {
+        clickById(Discover.ID_MAIN_TAB_DISCOVER);
+        scrollAndGetUIObject("WHO TO FOLLOW");
+        clickById(Discover.ID_MAIN_TAB_AD_SPALSH);
+    }
+    public void navToRecommendList(){
+        clickById(Discover.ID_MAIN_TAB_DISCOVER);
+
+    }
+    public void scrollAdSplash(){
+        clickById(Discover.ID_MAIN_TAB_DISCOVER);
+
+    }
+    public void scrollRecommendList(){
+        clickById(Discover.ID_MAIN_TAB_DISCOVER);
+    }
+    public boolean isExistedDiscover() throws UiObjectNotFoundException {
+        clickById(Discover.ID_MAIN_TAB_DISCOVER);
+        Boolean Results = scrollAndGetUIObject("WHO TO FOLLOW").getText().equals("WHO TO FOLLOW");
+        if (Results) {
+            Spoon.screenshot("Discover", "Click DiscoverCase");
+            Log.d("passed", "click Discover was passed");
+            return true;
+        } else {
+            Log.d("failed", "click 'Discover' to Discover failed!");
+            return false;
         }
     }
 }
