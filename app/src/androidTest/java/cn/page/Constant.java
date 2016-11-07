4affda1707c3bfb46259776045a2973c90477f20
tyokyo <qiang.zhang@ck-telecom.com>
2016-11-07 10:18:02 +0800

    add Constant

---
+++ app/src/androidTest/java/cn/page/Constant.java
@@ -0,0 +1,17 @@
+package cn.page;
+
+/**
+ * Created by elon on 2016/10/28.
+ */
+public class Constant {
+    //sio-eye account
+    public static  final  String userName ="tyokyo@126.com";
+    public static  final  String passwd   = "123456789";
+
+    //sio-eye error account
+    public static  final  String error_userName ="tyokyo@126.com";
+    public static  final  String error_passwd   = "0123456789";
+
+    //permission allow dialog
+    public static String PERMISSION_ALLOW="com.android.packageinstaller:id/permission_allow_button";
+}
