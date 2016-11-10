08d381895d3e5a6629a3cccde14b3692d7003ef0
tong.wang <tyokyo@126.com>
2016-11-10 20:28:57 +0800

    tong.wang

--- app/src/androidTest/java/usa/page/Device.java
+++ app/src/androidTest/java/usa/page/Device.java
@@ -4,4 +4,6 @@ package usa.page;
  * Created by elon on 2016/10/11.
  */
 public class Device {
+    //device tab
+    public static final String ID_device_Tab ="com.sioeye.sioeyeapp:id/main_tab_device";
 }
