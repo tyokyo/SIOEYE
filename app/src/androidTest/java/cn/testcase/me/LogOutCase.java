35353a6f74ca56e076cfd10c2af0ed3ea48dc931
qiang.zhang <qiang.zhang@ck-telecom.com>
2016-11-11 13:47:25 +0800

    add case for Me

---
+++ app/src/androidTest/java/cn/testcase/me/LogOutCase.java
@@ -0,0 +1,62 @@
+package cn.testcase.me;
+
+import android.support.test.filters.SdkSuppress;
+import android.support.test.runner.AndroidJUnit4;
+import android.support.test.uiautomator.By;
+import android.support.test.uiautomator.UiObjectNotFoundException;
+import android.support.test.uiautomator.Until;
+import com.squareup.spoon.Spoon;
+import org.hamcrest.Asst;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import ckt.base.VP2;
+import cn.page.AccountPage;
+import cn.page.App;
+import cn.page.Constant;
+import cn.page.MePage;
+
+/**
+ * Created by elon on 2016/10/28.
+ */
+@RunWith(AndroidJUnit4.class)
+@SdkSuppress(minSdkVersion = 16)
+public class LogOutCase extends VP2 {
+    @Before
+    public  void setup(){
+        openAppByPackageName(App.SIOEYE_PACKAGE_NAME_EN);
+    }
+    //注销Log out时->取消注销
+    @Test
+    public void testLogOut_Cancel(){
+        clickById(MePage.ID_MAIN_TAB_ME);
+        clickById(MePage.SETTINGS_USER_MAIN);
+        clickById(AccountPage.LOG_OUT);
+        clickById(AccountPage.LOG_OUT_CANCEL);
+        boolean active = getObjectById(AccountPage.LOG_OUT).exists();
+        Asst.assertEquals("logout cancel",true,active);
+    }
+    //注销Log out
+    @Test
+    public void testLogOut_OK() throws UiObjectNotFoundException {
+        //log out
+        clickById(MePage.ID_MAIN_TAB_ME);
+        clickById(MePage.SETTINGS_USER_MAIN);
+        clickById(AccountPage.LOG_OUT);
+        clickById(AccountPage.LOG_OUT_OK);
+        //log in
+        clickById(MePage.ID_MAIN_TAB_ME);
+        clickByText("登录");
+        //input username
+        getObjectById(MePage.INPUT_USERNAME).clearTextField();
+        getObjectById(MePage.INPUT_USERNAME).setText(Constant.userName);
+        //input password
+        getObjectById(MePage.INPUT_PASSWORD).clearTextField();
+        getObjectById(MePage.INPUT_PASSWORD).setText(Constant.passwd);
+        clickById(MePage.LOGIN_SIGN_UP);
+        gDevice.wait(Until.findObject(By.res(MePage.ID_MAIN_TAB_ME)),10000);
+        boolean active = getObjectById(MePage.ID_MAIN_TAB_ME).exists();
+        Asst.assertEquals("Login success",true,active);
+        Spoon.screenshot("Login_success");
+    }
+}
---
+++ app/src/androidTest/java/cn/testcase/me/NotificationCase.java
@@ -0,0 +1,69 @@
+package cn.testcase.me;
+
+import android.graphics.Rect;
+import android.support.test.filters.SdkSuppress;
+import android.support.test.runner.AndroidJUnit4;
+import android.support.test.uiautomator.By;
+import android.support.test.uiautomator.UiObjectNotFoundException;
+import android.support.test.uiautomator.Until;
+import com.squareup.spoon.Spoon;
+import org.hamcrest.Asst;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import ckt.base.VP2;
+import cn.action.AccountAction;
+import cn.action.MeAction;
+import cn.action.WatchAction;
+import cn.page.App;
+import cn.page.WatchPage;
+import usa.page.Me;
+
+/**
+ * Created by elon on 2016/10/31.
+ */
+@RunWith(AndroidJUnit4.class)
+@SdkSuppress(minSdkVersion = 16)
+public class NotificationCase extends VP2 {
+    public String username;
+    public int followed;
+    @Before
+    public  void setup() throws UiObjectNotFoundException {
+        openAppByPackageName(App.SIOEYE_PACKAGE_NAME_EN);
+        AccountAction.inLogin();
+    }
+    @Test
+    public void testNotificationAD() throws UiObjectNotFoundException {
+        MeAction.navToNotifications();
+        waitTime(3);
+        int f_users = gDevice.findObjects(By.res(Me.NOTIFICATIONS_USER_FOLLOW)).size();
+        if (f_users>=1) {
+            Rect rect = getObjectById(Me.NOTIFICATIONS_USER_FOLLOW).getBounds();
+            int width = gDevice.getDisplayWidth();
+            gDevice.click(width-rect.centerX(),rect.centerY());
+            waitUntilFind(WatchPage.WATCH_USER_MINI_NAME,40000);
+            username=getTex(WatchPage.WATCH_USER_MINI_NAME);
+            gDevice.pressBack();
+            clickById(Me.NOTIFICATIONS_USER_FOLLOW);
+            waitTime(2);
+            Spoon.screenshot("delete_all_who_followed_me_"+username);
+            gDevice.pressBack();
+            MeAction.navToFans();
+            followed = gDevice.findObjects(By.res(Me.USER_FOLLOW)).size();
+
+            Asst.assertEquals("delete_followed_user",f_users-1,followed);
+            Spoon.screenshot("delete_all_who_followed_me_"+username);
+            gDevice.pressBack();
+
+            /*//+Follow
+            MeAction.navToNotifications();
+            Rect rect1 = getUiObjectByTextContains(username).getBounds();
+            gDevice.click(rect1.left-10,rect1.centerY());
+            clickById(Watch.WATCH_USER_MINI_FOLLOW);
+            gDevice.pressBack();
+            int ff_users = gDevice.findObjects(By.res(Me.NOTIFICATIONS_USER_FOLLOW)).size();
+            Asst.assertEquals("delete_followed_user",followed+1,ff_users);
+            Spoon.screenshot("add_follow",username+"followed_me");*/
+        }
+    }
+}
