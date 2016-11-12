fc6fae6d7ec50e19cbddb6cdd237b44ca9519ab3
tong.wang <tyokyo@126.com>
2016-11-12 13:47:10 +0800

    rename

--- app/src/androidTest/java/usa/testcase/discover/SwitchTabCase.java
+++ app/src/androidTest/java/usa/testcase/discover/SwitchTabCase.java
@@ -20,7 +20,7 @@ import usa.page.Discover;
  */
 @RunWith(AndroidJUnit4.class)
 @SdkSuppress(minSdkVersion = 18)
-public class SwitchTab extends VP2 {
+public class SwitchTabCase extends VP2 {
     @Before
     public  void setup(){
         openAppByPackageName(App.SIOEYE_PACKAGE_NAME_USA);
