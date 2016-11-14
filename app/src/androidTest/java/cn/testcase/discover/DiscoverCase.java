5d4d9c6a23b5a28ba0a59b0d19799194937f81bf
qiang.zhang <qiang.zhang@ck-telecom.com>
2016-11-14 15:54:50 +0800

    add class

---
+++ app/src/androidTest/java/cn/testcase/discover/DiscoverCase.java
@@ -0,0 +1,142 @@
+package cn.testcase.discover;
+
+import android.graphics.Rect;
+import android.support.test.filters.SdkSuppress;
+import android.support.test.runner.AndroidJUnit4;
+import android.support.test.uiautomator.By;
+import android.support.test.uiautomator.UiObject;
+import android.support.test.uiautomator.UiObject2;
+import android.support.test.uiautomator.UiObjectNotFoundException;
+import android.support.test.uiautomator.UiSelector;
+import android.support.test.uiautomator.Until;
+import android.widget.ImageView;
+
+import com.squareup.spoon.Spoon;
+
+import org.hamcrest.Asst;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import java.io.IOException;
+import java.util.List;
+import java.util.logging.Logger;
+import ckt.base.VP2;
+import cn.action.AccountAction;
+import cn.action.BroadcastAction;
+import cn.action.DiscoverAction;
+import cn.action.MainAction;
+import cn.page.App;
+import cn.page.DiscoverPage;
+import cn.page.MePage;
+
+
+/**
+ * Created by caibing.yin on 2016/11/5.
+ */
+@RunWith(AndroidJUnit4.class)
+@SdkSuppress(minSdkVersion = 16)
+public class DiscoverCase extends VP2 {
+    Logger logger = Logger.getLogger(DiscoverCase.class.getName());
+    @Before
+    public  void setup() throws UiObjectNotFoundException {
+        openAppByPackageName(App.SIOEYE_PACKAGE_NAME_EN);
+        AccountAction.inLogin();
+    }
+    @Test
+   /**
+    * case1、单击搜索图标
+    * */
+    public void testOneClickSearch() throws IOException {
+        DiscoverAction.navToSearch();
+        waitTime(1);
+        if(!getObjectById(DiscoverPage.ID_SEARCH_FILTER_INPUT).exists()){
+            Spoon.screenshot("Go to SearchActivity Fail","跳转失败");
+            makeToast("跳转失败",5);
+            Assert.fail("跳转失败");
+        }
+    }
+    @Test
+    /**
+     * case2、双击搜索图标
+     */
+    public void testDoubleClickSearch() throws IOException {
+        clickById(DiscoverPage.ID_MAIN_TAB_DISCOVER);
+        UiObject2 frameLayout=getObject2ById(DiscoverPage.APP_TITLE).getParent();
+        UiObject2 searchObject=frameLayout.findObject(By.clazz(android.widget.ImageView.class));
+        Rect searchRect=searchObject.getVisibleBounds();
+        //double click
+        clickRect(searchRect);
+        clickRect(searchRect);
+        waitTime(1);
+        if(!getObjectById(DiscoverPage.ID_SEARCH_FILTER_INPUT).exists()){
+            Spoon.screenshot("Go to SearchActivity Fail","跳转失败");
+            makeToast("跳转失败",5);
+            Assert.fail("跳转失败");
+        }
+    }
+    @Test
+    /**
+     *case3、下拉刷新、在discover界面手指从上往下滑动
+     * 思路：通过判断刷新前后的推介列表的头像昵称是否相同来判定是否刷新，因为刷新后，
+     * 第一个头像的昵称是不一样的
+     */
+    public void testFlush() throws UiObjectNotFoundException, IOException {
+        clickById(DiscoverPage.ID_MAIN_TAB_DISCOVER);
+        waitTime(2);
+        // UiObject Discover
+        getObjectById(DiscoverPage.ID_DISCOVER_MAIN_CONTENT).swipeDown(50);
+        getObjectById(DiscoverPage.ID_DISCOVER_MAIN_CONTENT).swipeDown(50);
+        getObjectById(DiscoverPage.ID_DISCOVER_MAIN_CONTENT).swipeDown(50);
+        getObjectById(DiscoverPage.ID_DISCOVER_MAIN_CONTENT).swipeDown(50);
+        waitTime(2);
+        Spoon.screenshot("swipe_Down");
+        UiObject recommend_list = getObjectById(DiscoverPage.ID_MAIN_TAB_RECOMMAND_LIST);
+        String Expect_nickname = recommend_list.getChild(new UiSelector().index(0)).getText();
+        getObjectById(DiscoverPage.ID_DISCOVER_MAIN_CONTENT).swipeDown(50);
+        waitTime(2);
+        String Active_nickname = recommend_list.getChild(new UiSelector().index(0)).getText();
+        Boolean Result = Expect_nickname.equals(Active_nickname);
+        if(!Result) {
+            Spoon.screenshot("testFlush_fail", "下拉刷新失败");
+            makeToast("下拉刷新失败", 3);
+            Assert.fail("下拉刷新失败");
+        }
+        getObjectById(DiscoverPage.ID_DISCOVER_MAIN_CONTENT).swipeUp(50);
+        getObjectById(DiscoverPage.ID_DISCOVER_MAIN_CONTENT).swipeUp(50);
+        getObjectById(DiscoverPage.ID_DISCOVER_MAIN_CONTENT).swipeUp(50);
+        getObjectById(DiscoverPage.ID_DISCOVER_MAIN_CONTENT).swipeUp(50);
+        Spoon.screenshot("swipe_Up");
+    }
+    @Test
+    /**
+     *
+     *case4.来回频繁切换主界面
+     *Result：观察APP响应情况， APP迅速响应对应操作
+     */
+    public void testSwipe() throws UiObjectNotFoundException {
+        for (int i = 0; i <10 ; i++) {
+            MainAction.navToDiscover();
+            MainAction.navToMe();
+            MainAction.navToLive();
+            MainAction.navToDevice();
+            MainAction.navToDiscover();
+        }
+        Asst.assertEquals("navToDiscover",true,id_exists(MePage.ID_MAIN_TAB_DISCOVER));
+    }
+    //play the first video in discover page
+    @Test
+    public void testViewVideo() throws UiObjectNotFoundException {
+        MainAction.navToDiscover();
+        UiObject2 swipe_target=getObject2ById(DiscoverPage.ID_SWIPE_TARGET);
+        List<UiObject2> linearLayouts=swipe_target.findObjects(By.clazz(android.widget.RelativeLayout.class));
+        int size=linearLayouts.size();
+        linearLayouts.get(size-1).click();
+        BroadcastAction.waitBroadcastLoading();
+        gDevice.wait(Until.gone(By.res(MePage.BROADCAST_VIEW_VIDEO_LOADING)),120000);
+        Asst.assertEquals("加载2分钟",false,id_exists(MePage.BROADCAST_VIEW_VIDEO_LOADING));
+        Spoon.screenshot("testViewVideo");
+        gDevice.pressBack();
+    }
+}
+
---
+++ app/src/androidTest/java/cn/testcase/discover/RecommendCase.java
@@ -0,0 +1,218 @@
+package cn.testcase.discover;
+
+import android.graphics.Rect;
+import android.support.test.filters.SdkSuppress;
+import android.support.test.runner.AndroidJUnit4;
+import android.support.test.uiautomator.By;
+import android.support.test.uiautomator.UiObject2;
+import android.support.test.uiautomator.UiObjectNotFoundException;
+import android.widget.ImageView;
+import com.squareup.spoon.Spoon;
+import org.hamcrest.Asst;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import java.util.List;
+import java.util.logging.Logger;
+import ckt.base.VP2;
+import cn.action.AccountAction;
+import cn.action.DiscoverAction;
+import cn.page.App;
+import cn.page.DiscoverPage;
+
+/**
+ * Created by user on 2016/11/05   .
+ */
+@RunWith(AndroidJUnit4.class)
+@SdkSuppress(minSdkVersion = 16)
+public class RecommendCase extends VP2 {
+    Logger logger = Logger.getLogger(RecommendCase.class.getName());
+    @Before
+    public void setup() throws UiObjectNotFoundException {
+        openAppByPackageName(App.SIOEYE_PACKAGE_NAME_EN);
+        AccountAction.inLogin();
+    }
+
+    @Test
+    /**case1 单击推荐列表头像
+     * 唤出对应对象的个人资料页面；
+     * 单击推送列表好友后检查弹窗好友信息
+     */
+    public void testSingleClickRecommendList0() throws UiObjectNotFoundException {
+        //Single Check Recommend list 0单击推荐列表第一个人，检查弹出框PROFILE_MINI_HOME是否正确
+        String expect_name= DiscoverAction.navToRecommendList(0,1);
+        //两个参数分别为推荐列表第几个用户（0-3）和点击几次（1-2）
+        String active_name=getObjectById(DiscoverPage.ID_MAIN_TAB_PROFILE_MINI_NAME).getText();
+        Spoon.screenshot("testSingleClickRecommendList0");
+        Asst.assertEquals("点击的推荐用户名与弹出信息框是否一致",expect_name,active_name);
+        clickByClass("android.widget.ImageView",2);
+        //关闭弹出框
+        }
+    @Test
+    /**case2 双击推荐列表头像
+     * 唤出对应对象的个人资料页面；
+     * 双击推送列表好友后检查弹窗好友信息
+     */
+    public void testDoubleClickRecommend0() throws UiObjectNotFoundException {
+        //double Check Recommend list 0双击推荐列表第二个人，检查是否正确弹出框PROFILE_MINI_HOME是否正确
+        String expect_name=DiscoverAction.navToRecommendList(1,2);
+        //两个参数分别为推荐列表第几个用户（0-3）和点击几次（1-2）
+        String active_name=getObjectById(DiscoverPage.ID_MAIN_TAB_PROFILE_MINI_NAME).getText();
+        Spoon.screenshot("testDoubleClickRecommend0");
+        Asst.assertEquals("点击的推荐用户名与弹出信息框是否一致",expect_name,active_name);
+        clickByClass("android.widget.ImageView",2);
+        //关闭弹出框
+    }
+    @Test
+    /**case3添加推荐列表第一个为好友
+     * 检查该用户followers有没有增加;
+     * 检查自己好友列表有没有增加该好友
+     */
+    public void testAddDelFriendsRed() throws UiObjectNotFoundException{
+        String expect_name=DiscoverAction.navToRecommendList(1,1);
+        //两个参数分别为推荐列表第几个用户（0-3）和点击几次（1-2）
+        DiscoverAction.checkMiniProfileNumFollowerAddOneAfterFollow();
+        //验证是否添加成功
+        DiscoverAction.checkAddFriendsInMyFollowing(expect_name);
+        //取消关注
+        DiscoverAction.deleteFollowing(expect_name);
+        if (scrollAndGetUIObject(expect_name)!=null){
+            Spoon.screenshot("delete_user",expect_name);
+            Asst.fail("can not delete user success");
+        }
+    }
+    @Test
+    /**case4 滑动推荐列表后关注第二个推荐用户
+     * 检查该用户followers有没有增加;
+     *  并到Me关注好友去寻找是否添加成功
+     */
+    public void testSwipeRecommendAD() throws UiObjectNotFoundException{
+        DiscoverAction.scrollRecommendList();
+        String expect_name=DiscoverAction.navToRecommendList(1,1);
+        DiscoverAction.checkMiniProfileNumFollowerAddOneAfterFollow();
+        DiscoverAction.checkAddFriendsInMyFollowing(expect_name);
+        //取消关注
+        DiscoverAction.deleteFollowing(expect_name);
+        if (scrollAndGetUIObject(expect_name)!=null){
+            Spoon.screenshot("delete_user",expect_name);
+            Asst.fail("can not delete user success");
+        }
+    }
+    @Test
+    /**case5 检查nick name长度是否≤30
+     * [字体颜色，字体大小]无法实现，请检查截图或者
+     */
+    public void testCheckProfileMiniName() throws UiObjectNotFoundException{
+        String expect_name=DiscoverAction.navToRecommendList(1,1);
+        if (expect_name.length()>30){
+            Spoon.screenshot("testCheckrecommendProfileMiniName","Fail-Nick Name的字符串大于30个");
+            Assert.fail("Nick Name的字符串大于30个");
+        }
+        clickByClass("android.widget.ImageView",2);
+        //关闭弹出框
+    }
+    @Test
+    /**case6 刷新discover后检查推荐follow列表是否更新
+     * 检查刷新前后，前三个用户是否完全一致，判断是否刷新成功
+     */
+    public void testRefreshRecommendList() throws UiObjectNotFoundException{
+        String recommend_list_first_nickname_original=DiscoverAction.navToRecommendList(0,1);
+        clickByClass("android.widget.ImageView",2);
+        String recommend_list_second_nickname_original=DiscoverAction.navToRecommendList(1,1);
+        clickByClass("android.widget.ImageView",2);
+        String recommend_list_3rd_nickname_original=DiscoverAction.navToRecommendList(2,1);
+        clickByClass("android.widget.ImageView",2);
+        Spoon.screenshot("original_recommend_list","刷新前推荐列表");
+        getObjectById(DiscoverPage.ID_DISCOVER_MAIN_CONTENT).swipeDown(50);
+        waitTime(2);
+        String recommend_list_first_nickname_new=DiscoverAction.navToRecommendList(0,1);
+        clickByClass("android.widget.ImageView",2);
+        String recommend_list_second_nickname_new=DiscoverAction.navToRecommendList(1,1);
+        clickByClass("android.widget.ImageView",2);
+        String recommend_list_3rd_nickname_new=DiscoverAction.navToRecommendList(2,1);
+        clickByClass("android.widget.ImageView",2);
+        Spoon.screenshot("original_recommend_list","刷新后推荐列表");
+        if (recommend_list_first_nickname_original.equals(recommend_list_first_nickname_new)
+                &&recommend_list_second_nickname_original.equals(recommend_list_second_nickname_new)
+                &&recommend_list_3rd_nickname_original.equals(recommend_list_3rd_nickname_new)){
+            Spoon.screenshot("after_refresh");
+        }else{
+            Asst.fail("刷新后的推荐列表前三位与刷新前不一致，请检查图片");
+        }
+    }
+    @Test
+    /**case 7 Follow一个推荐列表好友后，刷新discover，
+     * 检查该好友是否从discover消失
+     */
+    public void testFollowThenRefresh() throws UiObjectNotFoundException{
+        String expect_name=DiscoverAction.navToRecommendList(1,1);
+        //两个参数分别为推荐列表第几个用户（0-3）和点击几次（1-2）
+        DiscoverAction.checkMiniProfileNumFollowerAddOneAfterFollow();
+        DiscoverAction.checkAddFriendsInMyFollowing(expect_name);
+        gDevice.pressBack();
+        clickById(DiscoverPage.ID_MAIN_TAB_DISCOVER);
+        Spoon.screenshot("recommend_list","推荐列表中有一个被Floowed");
+        getObjectById(DiscoverPage.ID_DISCOVER_MAIN_CONTENT).swipeDown(50);
+        waitTime(2);
+        String new_name=DiscoverAction.navToRecommendList(1,1);
+        if (expect_name.equals(new_name)){
+            Spoon.screenshot("recommend_list","推荐列表不应该有被Follwed");
+            Assert.fail("刷新后的推荐列表中被follow的用户没有消失");
+        }
+        //取消关注
+        DiscoverAction.deleteFollowing(expect_name);
+        if (scrollAndGetUIObject(expect_name)!=null){
+            Spoon.screenshot("delete_user",expect_name);
+            Asst.fail("can not delete user success");
+        }
+    }
+    @Test
+    /*
+    关闭推荐列表
+    点击推荐列表右上角关闭按钮
+    检查推荐列表是否存在
+     */
+    public void testCloseRedList() throws UiObjectNotFoundException{
+        clickById(DiscoverPage.ID_MAIN_TAB_DISCOVER);
+        waitTime(3);
+        String expect_name=DiscoverAction.navToRecommendList(1,1);
+        //关闭弹出框
+        clickByClass("android.widget.ImageView",2);
+        logger.info(expect_name);
+        Rect rect=getUiObjectByText("你可能感兴趣").getVisibleBounds();
+        int y=rect.centerY();
+        int x=gDevice.getDisplayWidth();
+        //你可能感兴趣-关闭按钮
+        gDevice.click(x-5,y);
+        boolean expect_name_exist=text_exists(expect_name);
+        Asst.assertEquals("关闭-你可能感兴趣",false,expect_name_exist);
+    }
+    @Test
+    /*
+    关闭推荐列表
+    点击推荐列表右上角关闭按钮
+    检查推荐列表是否存在
+    刷新列表
+    推荐列表恢复显示
+     */
+    public void testRecoverRedList() throws UiObjectNotFoundException{
+        clickById(DiscoverPage.ID_MAIN_TAB_DISCOVER);
+        waitTime(3);
+        String expect_name=DiscoverAction.navToRecommendList(1,1);
+        //关闭弹出框
+        clickByClass("android.widget.ImageView",2);
+        logger.info(expect_name);
+        Rect rect=getUiObjectByText("你可能感兴趣").getVisibleBounds();
+        int y=rect.centerY();
+        int x=gDevice.getDisplayWidth();
+        //你可能感兴趣-关闭按钮
+        gDevice.click(x-5,y);
+        boolean expect_name_exist=text_exists(expect_name);
+        Asst.assertEquals("关闭-你可能感兴趣",false,expect_name_exist);
+        getObjectById(DiscoverPage.ID_DISCOVER_MAIN_CONTENT).swipeDown(50);
+        waitTime(5);
+        expect_name_exist=text_exists(expect_name);
+        Asst.assertEquals("恢复-你可能感兴趣",true,expect_name_exist);
+    }
+}
