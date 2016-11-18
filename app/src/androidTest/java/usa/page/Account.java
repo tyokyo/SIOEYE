a31eb27c1e8b80fd80fca42025df7720c8bfaa4a
yun.yang <tyokyo@126.com>
2016-11-18 11:11:39 +0800

    page Account

--- app/src/androidTest/java/usa/page/Account.java
+++ app/src/androidTest/java/usa/page/Account.java
@@ -8,17 +8,21 @@ public class Account {
     public static final String LOG_OUT="com.sioeye.sioeyeapp:id/tv_logout";
     public static final String LOG_OUT_CANCEL="com.sioeye.sioeyeapp:id/delete_cancel";
     public static final String LOG_OUT_OK="com.sioeye.sioeyeapp:id/delete_ok";
-
+    //logout loading
+    public static final String ACCOUNT_LOGOUT="com.sioeye.sioeyeapp:id/llt_layout";
+    //绿色的登录按钮
+    public static final String ACCOUNT_LOGIN="com.sioeye.sioeyeapp:id/rl_root";
+    //输入的密码是否可见按钮
+    public static final String VISIBLE_AND_UNVISIBLE_PASSWORD="com.sioeye.sioeyeapp:id/show_password_btn";
+    public static final String ACCOUNT_INPUT_PHONE ="com.sioeye.sioeyeapp:id/et_input_phone";
     public static String logout_btn = "com.hicam:id/logout_btn";
     public static String login_id_input = "com.hicam:id/login_id_input";
     public static String login_password_input = "com.hicam:id/login_password_input";
     public static String login_btn_login = "com.hicam:id/login_btn_login";
-
     //login
     public static String LOGIN_ET_INPUT_USERNAME = "com.sioeye.sioeyeapp:id/et_input_username";
     public static String LOGIN_ET_INPUT_PASSWORD = "com.sioeye.sioeyeapp:id/et_input_password";
     public static String LOGIN_ET_SIGN_UP_BTN = "com.sioeye.sioeyeapp:id/login_signup";
-
     //sign up by mobile step
     public static String SIGN_UP_TEL_ET_INPUT_PHONE = "com.sioeye.sioeyeapp:id/et_input_phone";
     //error tip for sign up
@@ -32,9 +36,4 @@ public class Account {
     public static String SIGN_UP_ACCOUNT_DEFAULT_SELECT = "com.sioeye.sioeyeapp:id/tv_select_count";
     //pop dialog
     public static String SIGN_UP_ACCOUNT_TV_CONTENT = "com.sioeye.sioeyeapp:id/tv_content";
-    //logout loading
-    public static final String ACCOUNT_LOGOUT="com.sioeye.sioeyeapp:id/llt_layout";
-    //绿色的登录按钮
-    public static final String ACCOUNT_LOGIN="com.sioeye.sioeyeapp:id/rl_root";
-    public static final String ACCOUNT_INPUT_PHONE ="com.sioeye.sioeyeapp:id/et_input_phone";
 }
