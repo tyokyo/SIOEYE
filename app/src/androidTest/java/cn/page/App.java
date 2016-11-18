8f34745b989380384d885e8348d5e9187566cfae
qiang.zhang <qiang.zhang@ck-telecom.com>
2016-11-18 16:18:45 +0800

    comments

--- app/src/androidTest/java/cn/page/App.java
+++ app/src/androidTest/java/cn/page/App.java
@@ -4,8 +4,10 @@ package cn.page;
  * Created by elon on 2016/9/7.
  */
 public class App {
+    //USA 美国版
     public static String SIOEYE_PACKAGE_NAME_USA="cn.sioeye.sioeyeapp";
-    public static String SIOEYE_PACKAGE_NAME_EN="cn.sioeye.sioeyeapp";
+    //CN 中国版
+    public static String SIOEYE_PACKAGE_NAME_CN="cn.sioeye.sioeyeapp";
     public static String LAUNCH_SIOEYE="cn.sioeye.sioeyeapp/.main.SplashActivity";
 
 }
