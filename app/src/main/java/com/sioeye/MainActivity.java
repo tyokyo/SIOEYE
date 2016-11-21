802ca1a2f10d2ff0ac5083cc7d2daef140487a17
tyokyo <tyokyo@126.com>
2016-11-22 01:32:47 +0800

    add service

--- app/src/main/java/com/sioeye/MainActivity.java
+++ app/src/main/java/com/sioeye/MainActivity.java
@@ -1,12 +1,11 @@
 package com.sioeye;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Build;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import java.util.logging.Logger;
 
@@ -38,6 +37,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //启动Toast服务
+        Intent toastIntent = new Intent(MainActivity.this,ToastInfo.class);
+        startService(toastIntent);
+
         //版本大于6.0的情况
         if (Build.VERSION.SDK_INT >= 23) {
             int REQUEST_CODE_CONTACT = 101;
