530c84e72f149a766c06176be8dabc7fde1253df
qiang.zhang <qiang.zhang@ck-telecom.com>
2016-11-25 10:49:44 +0800

    comments

--- app/src/androidTest/java/cn/action/SettingAction.java
+++ app/src/androidTest/java/cn/action/SettingAction.java
@@ -12,73 +12,63 @@ import cn.page.MePage;
 public class SettingAction extends VP2 {
     //账号和安全
     public static void navToAccountAndPrivacy(){
-        clickById(MePage.ID_MAIN_TAB_ME);
-        clickById(MePage.SETTINGS_USER_MAIN);
+        MeAction.navToSettings();
         clickByText("账号与安全");
         Spoon.screenshot("AccountAndPrivacy");
     }
     //意见反馈
     public static void navToFeedBack(){
-        clickById(MePage.ID_MAIN_TAB_ME);
-        clickById(MePage.SETTINGS_USER_MAIN);
+        MeAction.navToSettings();
         clickByText("意见反馈");
         Spoon.screenshot("FeedBack");
     }
     //检查更新
     public static void navToUpdate(){
-        clickById(MePage.ID_MAIN_TAB_ME);
-        clickById(MePage.SETTINGS_USER_MAIN);
+        MeAction.navToSettings();
         clickByText("检查更新");
         Spoon.screenshot("checkUpdate");
     }
     //帮助中心
     public static void navToHelpCenter(){
-        clickById(MePage.ID_MAIN_TAB_ME);
-        clickById(MePage.SETTINGS_USER_MAIN);
+        MeAction.navToSettings();
         clickByText("帮助中心");
         Spoon.screenshot("HelpCenter");
     }
     //设置
     public static void navToSetting(){
-        clickById(MePage.ID_MAIN_TAB_ME);
-        clickById(MePage.SETTINGS_USER_MAIN);
+        MeAction.navToSettings();
         Spoon.screenshot("HelpCenter");
     }
     //关于SioEye
     public static void navToAboutSioEye(){
-        clickById(MePage.ID_MAIN_TAB_ME);
-        clickById(MePage.SETTINGS_USER_MAIN);
+        MeAction.navToSettings();
         clickByText("关于Sioeye");
         Spoon.screenshot("AboutSioeye");
     }
     //帮助中心->服务条款
     public static void navToHP_TermService(){
-        clickById(MePage.ID_MAIN_TAB_ME);
-        clickById(MePage.SETTINGS_USER_MAIN);
+        MeAction.navToSettings();
         clickByText("帮助中心");
         clickById(MePage.HELP_SERVICE);
         Spoon.screenshot("HELP_SERVICE");
     }
     //帮助中心->隐私策略
     public static void navToHP_Privacy(){
-        clickById(MePage.ID_MAIN_TAB_ME);
-        clickById(MePage.SETTINGS_USER_MAIN);
+        MeAction.navToSettings();
         clickByText("帮助中心");
         clickById(MePage.HELP_POLICY);
         Spoon.screenshot("HELP_POLICY");
     }
     //帮助中心->最终用户协议
     public static void navToHP_UserProtocol(){
-        clickById(MePage.ID_MAIN_TAB_ME);
-        clickById(MePage.SETTINGS_USER_MAIN);
+        MeAction.navToSettings();
         clickByText("帮助中心");
         clickById(MePage.HELP_EMULA);
         Spoon.screenshot("HELP_EMULA");
     }
     //帮助中心->帮助
     public static void navToHP_Help(){
-        clickById(MePage.ID_MAIN_TAB_ME);
-        clickById(MePage.SETTINGS_USER_MAIN);
+        MeAction.navToSettings();
         clickByText("帮助中心");
         clickById(MePage.HELP_HELP);
         Spoon.screenshot("HELP_HELP");
