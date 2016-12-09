13215d9a8c5d11d721da2f6c8ec22ad81eedca42
qiang.zhang <qiang.zhang@ck-telecom.com>
2016-12-09 15:54:06 +0800

    first version

---
+++ app/src/androidTest/java/iris4G/page/NavPage.java
@@ -0,0 +1,27 @@
+package iris4G.page;
+
+/**
+ * Created by elon on 2016/12/9.
+ */
+public class NavPage{
+    public static final String navConfig_LiveStream=Iris4GPage.nav_menu[0];
+    public static final String navConfig_Video      =Iris4GPage.nav_menu[1];
+    public static final String navConfig_Capture    =Iris4GPage.nav_menu[2];
+    public static final String navConfig_Burst      =Iris4GPage.nav_menu[3];
+    public static final String navConfig_Slo_Mo     =Iris4GPage.nav_menu[4];
+    public static final String navConfig_Lapse      =Iris4GPage.nav_menu[5];
+
+    public static final String angleSuperWide       =Iris4GPage.video_Angle[0];
+    public static final String angleWide             =Iris4GPage.video_Angle[1];
+    public static final String angleMedium           =Iris4GPage.video_Angle[2];
+
+    public static final String quality720_30         =Iris4GPage.video_quality[0];
+    public static final String quality720_60         =Iris4GPage.video_quality[1];
+    public static final String quality1080_30        =Iris4GPage.video_quality[2];
+
+    public static final String imageSize4M        =Iris4GPage.imsge_size[0];
+    public static final String imageSize3M        =Iris4GPage.imsge_size[1];
+    public static final String imageSize2M        =Iris4GPage.imsge_size[2];
+    public static final String imageSize8M        =Iris4GPage.imsge_size[3];
+
+}
