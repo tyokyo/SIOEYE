4550cb907bdb5175ae809ddb5642254f62e7ae97
caibing.yin <caibing.yin@ckt-telecom>
2016-12-10 14:10:25 +0800

    change case

--- app/src/androidTest/java/iris4G/testcase/ExchangeSetting_ReturnToLapse.java
+++ app/src/androidTest/java/iris4G/testcase/ExchangeSetting_ReturnToLapse.java
@@ -25,21 +25,21 @@ import static iris4G.page.Iris4GPage.video_quality;
 @RunWith(AndroidJUnit4.class)
 @SdkSuppress(minSdkVersion = 16)
 public class ExchangeSetting_ReturnToLapse extends VP2{
-    private String navConfig_LiveStream=Iris4GPage.nav_menu[0];
-    private String navConfig_Video=Iris4GPage.nav_menu[1];
-    private String navConfig_Capture=Iris4GPage.nav_menu[2];
-    private String navConfig_Burst=Iris4GPage.nav_menu[3];
-    private String navConfig_Slo_Mo=Iris4GPage.nav_menu[4];
-    private String navConfig_Lapse=Iris4GPage.nav_menu[5];
+//    private String navConfig_LiveStream=Iris4GPage.nav_menu[0];
+//    private String navConfig_Video=Iris4GPage.nav_menu[1];
+//    private String navConfig_Capture=Iris4GPage.nav_menu[2];
+//    private String navConfig_Burst=Iris4GPage.nav_menu[3];
+//    private String navConfig_Slo_Mo=Iris4GPage.nav_menu[4];
+//    private String navConfig_Lapse=Iris4GPage.nav_menu[5];
 
     Logger logger = Logger.getLogger(ExchangeSetting_ReturnToLapse.class.getName());
     @Before
     public void setup() throws Exception {
         Iris4GAction.initIris4G();
     }
-    public boolean isExistVideoQuality(String vquality){
-        boolean isexist =false;
-        return isexist;
+    public boolean isExistVideoQuality(String vQuality){
+        boolean isExist =false;
+        return isExist;
     }
     @Test
     public void  testExchangeSetting_ReturnToLapse() throws Exception  {
@@ -48,49 +48,49 @@ public class ExchangeSetting_ReturnToLapse extends VP2{
 
         for(int i=1;i<20;i++){
             logger.info("iteration-"+i);
-            int lapsesize = lapse_time.length;
-            int anglesize = video_Angle.length;
-            int qualitysize = video_quality.length;
-            int l =(int)(Math.random()*(lapsesize-1));
-            int a =(int)(Math.random()*(anglesize-1));
-            int q =(int)(Math.random()*(qualitysize-1));
+            int lapseSize = lapse_time.length;
+            int angleSize = video_Angle.length;
+            int qualitySize = video_quality.length;
+            int l =(int)(Math.random()*(lapseSize-1));
+            int a =(int)(Math.random()*(angleSize-1));
+            int q =(int)(Math.random()*(qualitySize-1));
 
             //value to set
             String expect_lapse_quality =video_quality[q];
             String expect_lapse_angle = video_Angle[a];
-            String expect_lapse_lapsetime = lapse_time[l];
+            String expect_lapse_lapseTime = lapse_time[l];
             //随即的视频质量当前机器是否支持,如果不支持，跳过
             if (CameraAction.isExistVideoQuality(expect_lapse_quality)) {
                 //action
                 String nacConfig =Iris4GPage.nav_menu[5];
                 CameraAction.configVideoQuality(nacConfig,expect_lapse_quality);
                 CameraAction.configVideoAngle(nacConfig,expect_lapse_angle);
-                CameraAction.configTimeLapse(nacConfig,expect_lapse_lapsetime);
+                CameraAction.configTimeLapse(nacConfig,expect_lapse_lapseTime);
 
                 CameraAction.navConfig(nacConfig);
                 CameraAction.cameraSetting();
 
                 //value get
-                String active_lapse_lapsetime = Iris4GAction.getRightValue("Time Lapse");
+                String active_lapse_lapseTime = Iris4GAction.getRightValue("Time Lapse");
                 String active_lapse_angle = Iris4GAction.getRightValue("Video Angle");
                 String active_lapse_quality = Iris4GAction.getRightValue("Video Quality");
 
                 logger.info("quality|"+expect_lapse_quality+"|"+active_lapse_quality);
                 logger.info("angle|"+expect_lapse_angle+"|"+active_lapse_angle);
-                logger.info("lapsetime|"+expect_lapse_lapsetime+"|"+active_lapse_lapsetime);
+                logger.info("lapseTime|"+expect_lapse_lapseTime+"|"+active_lapse_lapseTime);
 
-                if (!expect_lapse_lapsetime.equals(active_lapse_lapsetime)){
+                if (!expect_lapse_lapseTime.equals(active_lapse_lapseTime)){
                     logger.info(String.format("expect|active-[%s | %s]",
-                            expect_lapse_lapsetime,active_lapse_lapsetime));
-                    logger.info("testlapse_lapsetime_ReturnToLapseCaseCase_fail");
-                    Asst.fail(String.format("%s|%s",expect_lapse_lapsetime,active_lapse_lapsetime));
+                            expect_lapse_lapseTime,active_lapse_lapseTime));
+                    logger.info("testLapse_lapseTime_ReturnToLapseCaseCase_fail");
+                    Asst.fail(String.format("%s|%s",expect_lapse_lapseTime,active_lapse_lapseTime));
                     break;
                 }else {
                     if (!expect_lapse_angle.equals(active_lapse_angle)){
                         logger.info("Video Angle error");
                         logger.info("expect is:"+"["+expect_lapse_angle+"]");
                         logger.info("active is:"+"["+active_lapse_angle+"]");
-                        logger.info("testlapse_angle_ReturnToLapseCase_fail");
+                        logger.info("testLapse_angle_ReturnToLapseCase_fail");
                         Asst.fail();
                         break;
                     }else {
@@ -98,7 +98,7 @@ public class ExchangeSetting_ReturnToLapse extends VP2{
                             logger.info("Video Quality error");
                             logger.info("expect is:"+"["+expect_lapse_quality+"]");
                             logger.info("active is:"+"["+active_lapse_quality+"]");
-                            logger.info("testlapse_quality_ReturnToLapseCase_fail");
+                            logger.info("testLapse_quality_ReturnToLapseCase_fail");
                             Asst.fail();
                             break;
                         }
