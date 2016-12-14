27f336278226607cdfe758b3626b4cb0d9dd2772
ycbymy <ycb1991924>
2016-12-14 19:58:09 +0800

    12

--- app/src/androidTest/java/cn/testcase/discover/DiscoverCase.java
+++ app/src/androidTest/java/cn/testcase/discover/DiscoverCase.java
@@ -44,7 +44,6 @@ import cn.page.MePage;
 public class DiscoverCase extends VP2 {
     Logger logger = Logger.getLogger(DiscoverCase.class.getName());
     @Before
-
     public  void setup() throws UiObjectNotFoundException {
         openAppByPackageName(App.SIOEYE_PACKAGE_NAME_CN);
         AccountAction.inLogin();
