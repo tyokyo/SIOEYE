7362b12795b4a6661835b75c48393deabbdcd770
tong.wang <tyokyo@126.com>
2016-12-14 20:11:48 +0800

    添加聊天输入框ID

--- app/src/androidTest/java/cn/page/Other.java
+++ app/src/androidTest/java/cn/page/Other.java
@@ -5,5 +5,5 @@ public class Other {
 	public static String login_id_input = "com.hicam:id/login_id_input";
 	public static String login_password_input = "com.hicam:id/login_password_input";
 	public static String login_btn_login = "com.hicam:id/login_btn_login";
-
+	public static String chattextfield = "cn.sioeye.sioeyeapp:id/tipText";
 }
