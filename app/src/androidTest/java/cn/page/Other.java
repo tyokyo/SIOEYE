c69e19672ed59006e72b25301fbd288cd54610ac
tong.wang <tyokyo@126.com>
2016-12-15 19:01:11 +0800

    添加聊天输入框ID 添加微博按钮ID

--- app/src/androidTest/java/cn/page/Other.java
+++ app/src/androidTest/java/cn/page/Other.java
@@ -6,4 +6,5 @@ public class Other {
 	public static String login_password_input = "com.hicam:id/login_password_input";
 	public static String login_btn_login = "com.hicam:id/login_btn_login";
 	public static String chattextfield = "cn.sioeye.sioeyeapp:id/tipText";
+	public static String weibo_btn = "cn.sioeye.sioeyeapp:id/login_weibo";
 }
