837b7728eaaa1f82e1018d8de1a4f542103b58b6
yyun2016 <yun.yang@outlook.com>
2017-01-02 14:29:10 +0800

    add RegisterCase

--- app/src/androidTest/java/cn/testcase/other/RegisterCase.java
+++ app/src/androidTest/java/cn/testcase/other/RegisterCase.java
@@ -27,17 +27,16 @@ public class RegisterCase extends VP2 {
     @Before
     public void setup() throws UiObjectNotFoundException {
         openAppByPackageName(App.SIOEYE_PACKAGE_NAME_CN);
-        AccountAction.inLogin();
+        AccountAction.logOutAccount();
     }
     /**
-     * case20:
+     * case1:
      *电话号码注册
      *1、选择地区，输入合规范的手机号码进行注册
      *  Result：弹出Continue
      * */
     @Test
     public void testInvalidTelNumberSignUp() throws UiObjectNotFoundException, IOException {
-        AccountAction.logOutAccount();
         AccountAction.navToSignUp_ByMobile();
         String InvalidTelNumber = AccountAction.getRandomTel();
         for (int i = 0; i <11 ; i++) {
@@ -57,14 +56,13 @@ public class RegisterCase extends VP2 {
         }
     }
     /**
-     *case20:
+     *case2:
      *电话号码注册
      *1、选择地区，输入长度大于11位的手机号码进行注册
      *   Result：提示电话号码无效
      */
     @Test
     public void testInvalidTelNumber1SignUp() throws UiObjectNotFoundException, IOException {
-        AccountAction.logOutAccount();
         AccountAction.navToSignUp_ByMobile();
         String InvalidTelNumber = getRandomTel().concat(getRandomTel()) ;//得到22位的字符串
         shellInputText(getRandomTel());
@@ -79,7 +77,7 @@ public class RegisterCase extends VP2 {
         }
     }
     /**
-     *case22:
+     *case3:
      *Email注册
      *1、输入正确的Email进行注册
      *2、创建密码
@@ -89,7 +87,6 @@ public class RegisterCase extends VP2 {
      * */
     @Test
     public void testValidEmail() throws UiObjectNotFoundException, IOException {
-        AccountAction.logOutAccount();
         AccountAction.navToSignUp_ByEmail();
         String ValidEmail = AccountAction.getRandomEmail(3,15);
         shellInputText(ValidEmail);
@@ -100,14 +97,13 @@ public class RegisterCase extends VP2 {
         }
     }
     /**
-     *case22:
+     *case4:
      *Email注册
      *1、输入错误的Email进行注册
      *  Result：提示邮箱错误
      * */
     @Test
     public void testInvalidEmail() throws UiObjectNotFoundException, IOException {
-        AccountAction.logOutAccount();
         AccountAction.navToSignUp_ByEmail();
         String ValidEmail = AccountAction.getRandomEmail(3, 11);
         shellInputText(ValidEmail);
@@ -126,16 +122,12 @@ public class RegisterCase extends VP2 {
             Asst.fail("InValidEmailCase_fail");
         }
     }
-
-    /*
+    /* case5:
      *    2016/12/29 徐瑞香
      *    注册时输入12位不合法的电话号码
      */
-
     @Test
     public void err_mobile_reg() throws UiObjectNotFoundException {
-        AccountAction.logOutAccount();
-        //注销账号
         AccountAction.navToSignUp_ByMobile();
         //到手机注册界面
         clickById(AccountPage.SIGN_UP_TEL_ET_INPUT_PHONE);
@@ -146,10 +138,92 @@ public class RegisterCase extends VP2 {
         waitTime(2);
         Assert.assertEquals("注册无效的手机号码",true,getObjectById(AccountPage.SIGN_UP_ERROR_TIP).exists());
         Spoon.screenshot("Err_mobileNumber_Reg");
-
-
     }
+    /**By yun.yang 2017.1.2
+     *case6:
+     *Email注册,检查输入密码可见按钮是否有效
+     *1、输入随机Email进行注册
+     *2.在密码输入框中输入一串随机字符串（可见状态输入）
+     *  Result：可见
+     *  3.点击可见按钮
+     *  Result：不可见
+     *4.重新在密码输入框中输入一串随机字符串（不可见状态输入）
+     * Result：不可见
+     *5.点击可见按钮
+     * Result：可见
+     * */
+    @Test
+    public void testVisibleAndUnVisiblePasswordInRegisterByEmail() throws UiObjectNotFoundException, IOException {
+        AccountAction.navToInputPassword_ByEmailRegister();
+        AccountAction.checkVisibleAndUnVisiblePassword();
+    }
+/**By yun.yang 2017.1.2
+ *case7:
+ *Email注册,检查输入密码达到6位字符后有提示继续按钮
+ *1、输入随机Email进行注册
+ *2.输入密码达到6位时
+ *  Result：提示继续
+ * */
+     @Test
+     public void testPasswordDigitInRegisterByEmail() throws UiObjectNotFoundException, IOException {
+         AccountAction.navToInputPassword_ByEmailRegister();
+         String password=Constant.randomFixedLengthStringGenerator(5);
+         AccountAction.inputPassword(password);
+         if (getObjectByTextStartsWith("继续").exists()){
+             Spoon.screenshot("ItCanContinueWith5DigitPassword");
+             Assert.fail("ItCanContinueWith5DigitPassword");
+         }else {
+             clearText(AccountPage.SIGN_UP_ACCOUNT_PASSWORD_INPUT);
+             password=Constant.randomFixedLengthStringGenerator(6);
+             getObjectById(AccountPage.SIGN_UP_ACCOUNT_PASSWORD_INPUT).setText(password);
+             waitTime(2);
+             if (!getObjectByTextStartsWith("继续").exists()){
+                 Spoon.screenshot("ItCannotContinueWith6DigitPassword");
+                 Assert.fail("ItCan'tContinueWith6DigitPassword");
+             }
+         }
 
+     }
+
+/**By yun.yang 2017.1.2
+ *case8:
+ *Email注册,检查创建sioEyeID 4-15位有继续按钮
+ *1、输入随机Email进行注册（可见状态输入）
+ *2.在密码输入框中输入机字符串
+ *3.创建sioEyeID 4位  15位 +4-15中间随机一种
+ * Result：有继续按钮
+ * */
+@Test
+public void testSioEyeIDInRegisterByEmail() throws UiObjectNotFoundException, IOException {
+    AccountAction.navToInputPassword_ByEmailRegister();
+    String password = Constant.randomFixedLengthStringGenerator(8);
+    AccountAction.inputPassword(password);
+    clickById(AccountPage.SIGN_UP_CONTINUE);
+    String sioEye = Constant.randomFixedLengthStringGenerator(3);
+    AccountAction.inputSioEyeId(sioEye);
+    if (!getObjectById(AccountPage.SIOEYEID_VERIFIED_BTN).exists()) {
+        Spoon.screenshot("ItCanContinueWith3DigitSioEye");
+        Assert.fail("ItCanContinueWith3DigitSioEye");
+    } else {
+        clearText(AccountPage.SIGN_UP_ACCOUNT_SIOEYE_ID);
+        sioEye = Constant.randomFixedLengthStringGenerator(4);
+        getObjectById(AccountPage.SIGN_UP_ACCOUNT_SIOEYE_ID).setText(sioEye);
+        waitTime(2);
+        if (!getObjectByTextStartsWith("继续").exists()) {
+            Spoon.screenshot("CantContinueWith4DigitSioEyeID");
+            Assert.fail("CantContinueWith4DigitSioEyeID");
+        } else {
+            clearText(AccountPage.SIGN_UP_ACCOUNT_SIOEYE_ID);
+            sioEye = Constant.randomFixedLengthStringGenerator(15);
+            getObjectById(AccountPage.SIGN_UP_ACCOUNT_SIOEYE_ID).setText(sioEye);
+            waitTime(2);
+            if (!getObjectById(AccountPage.SIOEYEID_VERIFIED_BTN).exists()) {
+                Spoon.screenshot("ItCanContinueWith3DigitSioEye");
+                Assert.fail("ItCanContinueWith3DigitSioEye");
+            }
+        }
+    }
+}
 
 
 }
