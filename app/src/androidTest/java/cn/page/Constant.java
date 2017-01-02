244dad3922c9cf99faec1f39e51521d71480ae96
yyun2016 <yun.yang@outlook.com>
2017-01-02 17:31:14 +0800

    优化constant方法

--- app/src/androidTest/java/cn/page/Constant.java
+++ app/src/androidTest/java/cn/page/Constant.java
@@ -112,8 +112,7 @@ public class Constant {
                 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v','K',
                 'w', 'x', 'y', 'z','A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
                 'J','L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V','W',
-                'X', 'Y', 'Z','0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-                '@', '#', '$','%', '&', '*', ':', ';', '/', '?', '-', '+', '=', };
+                'X', 'Y', 'Z','0', '1', '2', '3', '4', '5', '6', '7', '8', '9','_'};
         String randomSequence = "";
         for (int i = 0; i < length; i++) {
             Character c = Character.valueOf(alphaArray[new Random().nextInt(alphaArray.length)]);
