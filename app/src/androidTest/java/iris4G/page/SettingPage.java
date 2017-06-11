d30e2d15516cf381259ebac4534fb60a4d0019f8
yyun2016 <yun.yang@outlook.com>
2017-06-11 20:26:59 +0800

    add  SettingPage

---
+++ app/src/androidTest/java/iris4G/page/SettingPage.java
@@ -0,0 +1,12 @@
+package iris4G.page;
+
+import ckt.base.VP2;
+
+/**
+ * Created by yyun on 2017/6/11.
+ */
+
+public class SettingPage extends VP2{
+    public static String storage_used_tx = "com.android.settings:id/storage_used_tx";
+    public static String storage_avial_tx ="com.android.settings:id/storage_avial_tx";
+}
