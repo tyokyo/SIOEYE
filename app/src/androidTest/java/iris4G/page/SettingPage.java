498af628b5c6f979b333b42d3aa120a72093268d
yyun2016 <yun.yang@outlook.com>
2017-06-12 18:09:18 +0800

    add setting page

--- app/src/androidTest/java/iris4G/page/SettingPage.java
+++ app/src/androidTest/java/iris4G/page/SettingPage.java
@@ -9,4 +9,8 @@ import ckt.base.VP2;
 public class SettingPage extends VP2{
     public static String storage_used_tx = "com.android.settings:id/storage_used_tx";
     public static String storage_avial_tx ="com.android.settings:id/storage_avial_tx";
+    public static String storage_delete_img ="com.android.settings:id/storage_delete_img";
+    public static String wifi_connected_state="com.android.settings:id/state";
+    public static String wifi_switch_state="com.android.settings:id/switchState";
+    public static String category_content="com.android.settings:id/category_content";
 }
