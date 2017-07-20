9b918bb6039ef3e8d81b6368aaa2510d386ef544
zyj <yajuan.zhang@ck-telecom.com>
2017-07-20 19:59:01 +0800

    add page

---
+++ app/src/androidTest/java/cn/page/NewPage.java
@@ -0,0 +1,28 @@
+package cn.page;
+
+/**
+ * Created by yajuan on 2017/7/17.
+ */
+
+public class NewPage {
+    //搜索
+    public static final String ID_NEW_SEARCH_INPUT = "cn.sioeye.sioeyeapp:/search_filter_input";
+
+    public static final String ID_MAIN_TAB_DISCOVER = "cn.sioeye.sioeyeapp:id/main_tab_discover";
+    //个人详情弹出框中
+    public static final String ID_NEW_PROFILE_MINI_NAME ="cn.sioeye.sioeyeapp:id/tv_nickname";
+    public static final String ID_NEW_PROFILE_MINI_NUM_FOLLOWER ="cn.sioeye.sioeyeapp:id/profile_mini_num_follower_text";
+    //加关注
+    public static final String ID_MAIN_TAB_PROFILE_MINI_NUM_FOLLOW ="cn.sioeye.sioeyeapp:id/profile_mini_follow";
+    //弹出框中昵称和性别
+    public static final String ID_NEW_PROFILE_NAME = "cn.sioeye.sioeyeapp:id/llt_name";
+    //主播头像
+    public static final String ID_NEW_AVATOR = "cn.sioeye.sioeyeapp:id/media_avatar";
+    //视频列表
+    public static final String ID_NEW_VIDEO = "cn.sioeye.sioeyeapp:id/newest_list";
+
+    //播放界面
+    public static final String NEW_VIEW_VIDEO="cn.sioeye.sioeyeapp:id/video_view1";
+    //聊天室
+    public static final String NEW_VIEW_CHAT="cn.sioeye.sioeyeapp:id/watch_pager";
+}
