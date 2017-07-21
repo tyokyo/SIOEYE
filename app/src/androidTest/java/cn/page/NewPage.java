2d420b7dc3d225ea8f474f94159fe8582fe1273d
fanzhengxiong <zhengxiong.fan@ck-telecom.com>
2017-07-21 11:19:14 +0800

    add vediolist

--- app/src/androidTest/java/cn/page/NewPage.java
+++ app/src/androidTest/java/cn/page/NewPage.java
@@ -20,7 +20,7 @@ public class NewPage {
     public static final String ID_NEW_AVATOR = "cn.sioeye.sioeyeapp:id/media_avatar";
     //视频列表
     public static final String ID_NEW_VIDEO = "cn.sioeye.sioeyeapp:id/newest_list";
-
+    public static final String ID_New_Vediolist ="cn.sioeye.sioeyeapp:id/discover_newest_swipe";
     //播放界面
     public static final String NEW_VIEW_VIDEO="cn.sioeye.sioeyeapp:id/video_view1";
     //聊天室
