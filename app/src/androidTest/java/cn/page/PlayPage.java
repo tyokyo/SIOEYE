5be51c49dd8331272fc58cbc3caf03f96940bb80
zyj <yajuan.zhang@ck-telecom.com>
2017-08-09 15:22:35 +0800

    add page

---
+++ app/src/androidTest/java/cn/page/PlayPage.java
@@ -0,0 +1,49 @@
+package cn.page;
+
+/**
+ * Created by admin on 2017/8/8.
+ * 播放界面的id信息
+ */
+
+public class PlayPage {
+    //日期
+    public static final String PLAY_ABOUT_TIME="cn.sioeye.sioeyeapp:id/tv_date";
+    //主播
+    public static final String TV_AUCHOR_ID="cn.sioeye.sioeyeapp:id/tv_anchor";
+    //聊天室
+    public static final String TV_CHAT_ROOM_ID="cn.sioeye.sioeyeapp:id/tv_chat_room";
+    //底部有新消息
+    public static final String NEW_MESSAGES_DISPLAY="cn.sioeye.sioeyeapp:id/tv_new_message";
+    //视频加载失败
+    public static final String LOAD_VIDEO_ERROR="cn.sioeye.sioeyeapp:id/layout_error";
+    //视频信息
+    public static final String VIDEO_WATCH_NUMBER="cn.sioeye.sioeyeapp:id/tv_watch_number";
+    public static final String VIDEO_CHAT_NUMBER="cn.sioeye.sioeyeapp:id/tv_chat_number";
+    public static final String VIDEO_LIKE_NUMBER="cn.sioeye.sioeyeapp:id/tv_like_number";
+    //video loading
+    public static final String BROADCAST_VIEW_VIDEO_LOADING="cn.sioeye.sioeyeapp:id/app_video_loading";
+    //play video
+    public static final String BROADCAST_VIEW_VIDEO_PLAY="cn.sioeye.sioeyeapp:id/app_video_play";
+    //current video time
+    public static final String BROADCAST_VIEW_VIDEO_CURRENT_TIME="cn.sioeye.sioeyeapp:id/app_video_currentTime";
+    //播放进度
+    public static final String BROADCAST_VIEW_VIDEO_SEEKBAR="cn.sioeye.sioeyeapp:id/app_video_seekBar";
+    //video end time
+    public static final String BROADCAST_VIEW_VIDEO_END_TIME="cn.sioeye.sioeyeapp:id/app_video_endTime";
+    //全屏播放按钮
+    public static final String BROADCAST_VIEW_VIDEO_FULL_SCREEN="cn.sioeye.sioeyeapp:id/app_video_fullscreen";
+    //简介界面
+    public static final String PLAY_ABOUT="cn.sioeye.sioeyeapp:id/anchor_recyclerView";
+    //键盘弹出时的点赞图标
+    public static final String BROADCAST_VIEW_ZAN_FLOAT_LIKE="cn.sioeye.sioeyeapp:id/float_like";
+
+    public static final String BROADCAST_VIEW_SHARE="cn.sioeye.sioeyeapp:id/iv_share";
+
+    public static final String BROADCAST_VIEW_MESSAGE_TEXT="cn.sioeye.sioeyeapp:id/message_text";
+    public static final String BROADCAST_VIEW_WATCHER_COUNT="cn.sioeye.sioeyeapp:id/watch_player_portrait_watcher_count";
+    public static final String BROADCAST_VIEW_TIPTEXT="cn.sioeye.sioeyeapp:id/tipText";
+    public static final String BROADCAST_VIEW_ZAN="cn.sioeye.sioeyeapp:id/watch_player_portrait_like";
+    //键盘输入框
+    public static final String EDIT_COMMENT_TEXT="cn.sioeye.sioeyeapp:id/edit_text";
+
+}
