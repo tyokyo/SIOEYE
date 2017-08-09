d9b29ba925b4541276e68869d04fa81152017a92
zyj <yajuan.zhang@ck-telecom.com>
2017-08-09 15:13:01 +0800

    change page

--- app/src/androidTest/java/cn/action/FollowersAction.java
+++ app/src/androidTest/java/cn/action/FollowersAction.java
@@ -1,18 +1,18 @@
 package cn.action;
 
 import ckt.base.VP2;
-import cn.page.MePage;
+import cn.page.PlayPage;
 
 /**
  * Created by elon on 2016/11/16.
  */
 public class FollowersAction extends VP2 {
     public static void clickToChat(){
-        clickById(MePage.TV_CHAT_ROOM_ID);
+        clickById(PlayPage.TV_CHAT_ROOM_ID);
         //clickByText("聊天室");
     }
     public static void clickToAnchor(){
-        clickById(MePage.TV_AUCHOR_ID);
+        clickById(PlayPage.TV_AUCHOR_ID);
         //clickByText("主播");
     }
 }
