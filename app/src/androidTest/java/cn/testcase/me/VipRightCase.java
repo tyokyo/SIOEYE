dc5f5ffb491ba6560f2e75722db73f0c8845b529
zyj <yajuan.zhang@ck-telecom.com>
2017-09-14 10:14:50 +0800

    change case

--- app/src/androidTest/java/cn/testcase/me/VipRightCase.java
+++ app/src/androidTest/java/cn/testcase/me/VipRightCase.java
@@ -3,24 +3,17 @@ package cn.testcase.me;
 import ckt.base.VP2;
 import android.support.test.filters.SdkSuppress;
 import android.support.test.runner.AndroidJUnit4;
-import android.support.test.uiautomator.By;
+import android.support.test.uiautomator.Direction;
 import android.support.test.uiautomator.UiObjectNotFoundException;
-
 import com.squareup.spoon.Spoon;
-
 import org.hamcrest.Asst;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
-import java.io.IOException;
-
 import ckt.annotation.PerformanceTest;
 import ckt.annotation.SanityTest;
-
 import cn.action.AccountAction;
 import cn.action.MeAction;
-import cn.action.SettingAction;
 import cn.page.App;
 import cn.page.MePage;
 
@@ -64,11 +57,10 @@ public class VipRightCase extends VP2 {
     @PerformanceTest
     public void testPurchase() throws UiObjectNotFoundException {
         MeAction.navToVipRights();
+        getObject2ById(MePage.VIP_RIGHT).swipe(Direction.UP,0.5f);
         clickById(MePage.VIP_RIGHT_TV_OPEN);
         String expect_name1="WeChat Pay";
         String active_name1=getObjectById(MePage.VIP_RIGHT_TV_CONTENT).getText();
-        //String expect_name2="Alipay";
-        //String active_name2=getObjectById(MePage.VIP_RIGHT_TV_CONTENT).getText();
         String expect_name3="Cancel";
         String active_name3=getObjectById(MePage.VIP_RIGHT_TV_CHOOSE_CANCEL).getText();
         Asst.assertEquals("点击的信息是否一致", expect_name1+expect_name3, active_name1+active_name3);
