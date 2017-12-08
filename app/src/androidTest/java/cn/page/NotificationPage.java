bac6298af5898de23721ef0cb2af7af89e4ad658
qiang.zhang <qiang.zhang@ck-telecom.com>
2017-12-08 10:20:17 +0800

    add page

---
+++ app/src/androidTest/java/cn/page/NotificationPage.java
@@ -0,0 +1,14 @@
+package cn.page;
+
+/**
+ * Created by qiang.zhang on 2017/12/7.
+ */
+
+public class NotificationPage {
+    //通知消息列表
+    public static String NTF_LIST = "cn.sioeye.sioeyeapp:id/list";
+    public static String NTF_NICK_NAME = "cn.sioeye.sioeyeapp:id/tv_nickname";
+    public static String NTF_VIDEO_INFO = "cn.sioeye.sioeyeapp:id/llt_video_info";
+    public static String NTF_FOLLOW="cn.sioeye.sioeyeapp:id/profile_mini_follow";
+
+}
